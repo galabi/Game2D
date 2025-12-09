3cffb9c597797bba356fd670044747c9780ca845
galabi <galabisira@gmail.com>
2025-12-09 22:26:03 +0200

    update to regenarion maneger

--- src/Regeneration/RegenerationBlock.java
+++ src/Regeneration/RegenerationBlock.java
@@ -1,17 +1,21 @@
 package Regeneration;
 
 import Entity.Object;
+import MainPackage.TilesManager;
 
 public class RegenerationBlock {
-	private int x,y,nextId;
+	private int x,y,mapI,mapJ;
 	private Object objectToGrow;
 	private long GrowthTime;
+	private String targetGrow;
 	
-	public RegenerationBlock(Object objectToGrow,int x,int y,int nextId) {
+	public RegenerationBlock(Object objectToGrow,int x,int y,String targetGrow) {
 		this.objectToGrow = objectToGrow;
-		this.nextId = nextId;
+		this.targetGrow = targetGrow;
 		this.x = x;
 		this.y = y;
+		this.mapI = y/TilesManager.tileSize;
+		this.mapJ = x/TilesManager.tileSize;
 		setNextGowthTime();
 	}
 	
@@ -21,19 +25,29 @@ public class RegenerationBlock {
 	public int getY() {
 		return y;
 	}
+	public int getMapI() {
+		return mapI;
+	}
+	public int getMapJ() {
+		return mapJ;
+	}
 	public Object getObject() {
 		return objectToGrow;
 	}
 	public long getGrowthTime() {
 		return GrowthTime;
 	}
-	public int getNextId() {
-		return nextId;
+	public String getTargetGrow() {
+		return targetGrow;
 	}
-	public void setNextId(int nextId) {
-		this.nextId = nextId;
+	public void setTargetGrow(String targetGrow) {
+		this.targetGrow = targetGrow;
 	}
 	public void setNextGowthTime() {
 		GrowthTime = RegenerationManager.getNextGowthTime() ;
+	}
+	
+	public boolean isReadyToGrow() {
+		return (GrowthTime <= System.currentTimeMillis());
 	}
 }
--- src/Regeneration/RegenerationManager.java
+++ src/Regeneration/RegenerationManager.java
@@ -4,63 +4,96 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import Entity.Object;
+import MainPackage.Main;
+import MainPackage.TilesManager;
 import mutiplayer.ServerClientHandler;
 
 public class RegenerationManager {
 	
-	final private static int maxGrowIdRock = 33,maxGrowIdTree = 1;
-	static ArrayList<RegenerationBlock> regenereationList = new ArrayList<>();;
+	static ArrayList<RegenerationBlock> regenerationList = new ArrayList<>();
 	
 	public static void tick() {
-		if(regenereationList.isEmpty()) return;
+		
+		if(regenerationList.isEmpty()) return;
 			
-		RegenerationBlock temp = regenereationList.get(0);
-		if(temp.getGrowthTime() < System.currentTimeMillis()) {
-			temp.getObject().setType(temp.getNextId());
-			ServerClientHandler.sendDataToServer("update_block " + temp.getY() + " " + temp.getX() + " " + temp.getObject().getId());
-			if(temp.getObject().getId() == maxGrowIdRock || temp.getObject().getId() == maxGrowIdTree) {
-				regenereationList.remove(0);
-			}else {
-				temp.setNextId(temp.getNextId()+1);
-				temp.setNextGowthTime();
+		RegenerationBlock temp = regenerationList.get(0);
+		
+		if(temp.isReadyToGrow()) {
+			if(temp.getTargetGrow().equals("Tree")){
+				GrowTree(temp.getMapI(), temp.getMapJ(), Main.tilesManager.getObjects());
+			}else if(temp.getTargetGrow().equals("Rock Max")) {
+				GrowRock(temp.getMapI(), temp.getMapJ(), Main.tilesManager.getObjects());
 			}
+			
+			regenerationList.remove(0);
+			
 		}
 		
 	}
 	
 	//add the object to the growth list;
 	public static void insertToGrowthList(Object nextObject,int x,int y) {
-		int nextId = 0;
+		String targetGrow = "";
+		
 		//tree case
-		if(nextObject.getId() == 2) {
-			nextId = 1;
-		//rock case
-		}else if(nextObject.getId() >= 30 && nextObject.getId() <maxGrowIdRock) {
-			nextId = nextObject.getId()+1;
-			
-			//check if the rock is in the list, so it won't add the same block to the list
-			for(RegenerationBlock i : regenereationList) {
-				if(i.getX() == x && i.getY() == y) {
-					i.setNextGowthTime();
-					i.setNextId(nextId);
-					return;
-				}
-			}
-		}else {
-			return;
+		if(nextObject.getName().equals("Tree sapling")) {
+			targetGrow = "Tree";
+		}else if(nextObject.getName().equals("Rock")) {
+			targetGrow = "Rock Max";
 		}
-		regenereationList.add(new RegenerationBlock(nextObject,x,y,nextId));
+		regenerationList.add(new RegenerationBlock(nextObject,x,y,targetGrow));
 	}
 	
 	public static long getNextGowthTime() {
-		if(regenereationList.isEmpty()) {
+		if(regenerationList.isEmpty()) {
 			return System.currentTimeMillis() + new Random().nextInt(30000,50000);
 		}
-		return regenereationList.get(regenereationList.size()-1).getGrowthTime()+ new Random().nextInt(10000,30000);
+		return regenerationList.get(regenerationList.size()-1).getGrowthTime()+ new Random().nextInt(10000,30000);
 	}
 	
 	public static void resetList() {
-		regenereationList.clear();
+		regenerationList.clear();
+	}
+	
+	
+	private static void GrowTree(int rootMapI,int rootMapJ,Object[][] objectsMap) {
+		//tree base
+		objectsMap[rootMapI][rootMapJ].setType(14); 
+		objectsMap[rootMapI-1][rootMapJ].setType(2); 
+		
+		//left tree 
+		if(objectsMap[rootMapI][rootMapJ-1].getName().equals("Tree")) {
+			objectsMap[rootMapI][rootMapJ-1].setType(17);
+			objectsMap[rootMapI-1][rootMapJ-1].setType(5);
+		}else {
+			objectsMap[rootMapI][rootMapJ-1].setType(13);
+			objectsMap[rootMapI-1][rootMapJ-1].setType(1);
+		}
+		
+		//right tree
+		if(objectsMap[rootMapI][rootMapJ+1].getName().equals("Tree")) {
+			objectsMap[rootMapI][rootMapJ+1].setType(17);
+			objectsMap[rootMapI-1][rootMapJ+1].setType(5);
+		}else {
+			objectsMap[rootMapI][rootMapJ+1].setType(15);
+			objectsMap[rootMapI-1][rootMapJ+1].setType(3);
+		}
+		
+		ServerClientHandler.sendDataToServer("update_block " + rootMapJ*TilesManager.tileSize + " " + rootMapI*TilesManager.tileSize + " " + objectsMap[rootMapI][rootMapJ].getId());
+		ServerClientHandler.sendDataToServer("update_block " + rootMapJ*TilesManager.tileSize + " " + (rootMapI-1)*TilesManager.tileSize + " " + objectsMap[rootMapI-1][rootMapJ].getId());
+		ServerClientHandler.sendDataToServer("update_block " + (rootMapJ+1)*TilesManager.tileSize + " " + rootMapI*TilesManager.tileSize + " " + objectsMap[rootMapI][rootMapJ+1].getId());
+		ServerClientHandler.sendDataToServer("update_block " + (rootMapJ+1)*TilesManager.tileSize + " " + (rootMapI-1)*TilesManager.tileSize + " " + objectsMap[rootMapI-1][rootMapJ+1].getId());
+		ServerClientHandler.sendDataToServer("update_block " + (rootMapJ-1)*TilesManager.tileSize + " " + rootMapI*TilesManager.tileSize + " " + objectsMap[rootMapI][rootMapJ-1].getId());
+		ServerClientHandler.sendDataToServer("update_block " + (rootMapJ-1)*TilesManager.tileSize + " " + (rootMapI-1)*TilesManager.tileSize + " " + objectsMap[rootMapI-1][rootMapJ-1].getId());
+		
+	}
+	
+	private static void GrowRock(int rockMapI,int rockMapJ,Object[][] objectsMap) {
+		objectsMap[rockMapI][rockMapJ].setType(objectsMap[rockMapI][rockMapJ].getId()-1);
+		if(objectsMap[rockMapI][rockMapJ].getName().equals("Rock Max") == false) {
+			insertToGrowthList(objectsMap[rockMapI][rockMapJ],rockMapJ*TilesManager.tileSize,rockMapI*TilesManager.tileSize);
+		}
+		ServerClientHandler.sendDataToServer("update_block " + rockMapJ*TilesManager.tileSize + " " + rockMapI*TilesManager.tileSize + " " + objectsMap[rockMapI][rockMapJ].getId());
 	}
 	
 }
